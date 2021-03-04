3fa7a53782199a7b4cdeb4c33d64451761e5a907
Marcelo8173 <marceloandrebio@gmail.com>
2021-03-03 22:33:07 -0300

    create styles and test styles

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -1,8 +1,14 @@
+import * as S from './styles';
+
 const Main = () => {
     return (
-        <main>
-            <h1>React avançado</h1>
-        </main>
+        <S.Wrapper>
+            <S.Logo src="/img/logo.svg" alt="Imagem de um atomo e React Avançado escrito ao lado."/>
+            <S.Title>React avançado</S.Title>
+            <S.Description>TypeScript, ReactJS, NextJS e Styled componet</S.Description>
+            <S.Ilustration src="/img/hero-illustration.svg" alt="um desenvolvedor de frente para tela com 
+            um codigo"/>
+        </S.Wrapper>
     );
 }
 
--- src/components/main/test.tsx
+++ src/components/main/test.tsx
@@ -9,4 +9,10 @@ describe('<Main/>', () => {
             screen.getByRole('heading', { name: /react avançado/i })
         ).toBeInTheDocument
     });
+
+    it('should render color correct', () => {
+        const { container } = render(<Main />);
+
+        expect(container.firstChild).toHaveStyle({ 'background': '#06092b' });
+    })
 })
