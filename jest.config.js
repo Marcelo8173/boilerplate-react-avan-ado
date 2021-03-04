f2a56ded315492656adbde81c987e6d3c42911fd
Marcelo8173 <marceloandrebio@gmail.com>
2021-03-04 20:39:17 -0300

    fix jest setup

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,8 @@ module.exports = {
     testPathIgnorePatterns: ['/node_modules/', '/.next/'],
     collectCoverage: true,
     collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/stories.tsx'],
-    setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
-    modulePaths: ['<rootDir>/src/']
+    setupFilesAfterEnv: ['<rootDir>/.jest/jest-setup.ts'],
+    modulePaths: ['<rootDir>/src/'],
+    
   }
   
