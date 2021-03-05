1c4f67e23caad129386584de34b2e61c9cc000cc
Marcelo8173 <marceloandrebio@gmail.com>
2021-03-04 21:48:51 -0300

    insert types

--- src/components/main/main.stories.tsx
+++ src/components/main/main.stories.tsx
@@ -1,13 +1,14 @@
 import { text, withKnobs } from '@storybook/addon-knobs';
+import { Meta, Story} from '@storybook/react/types-6-0';
 import Main from '.';
 
 export default {
     title: 'Main',
     component: Main,
     decorators: [withKnobs]
-}
+} as Meta;
 
-export const Basica = () => {
+export const Basica: Story = () => {
     return (
         <Main
             title={text('title', 'Teste de React Avançado')}
